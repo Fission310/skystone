d8ea34d262b9040606cf5be55e85e69016a46e76
Lucas Lee <lucasclee11@gmail.com>
2019-12-07 15:39:36 -0500

    outer red

---
+++ opmode/auton/OuterParkMoveFoundationRed.java
@@ -0,0 +1,49 @@
+package org.firstinspires.ftc.teamcode.opmode.auton;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.hardware.Acquirer;
+import org.firstinspires.ftc.teamcode.hardware.Drivetrain;
+import org.firstinspires.ftc.teamcode.hardware.Platform;
+
+
+//@Config
+@Autonomous(name = "OuterParkMoveFoundationRed")
+public class OuterParkMoveFoundationRed extends LinearOpMode {
+
+    public static double distance = 22;
+
+    Drivetrain drive = new Drivetrain(this);
+    Platform platform = new Platform(this);
+    Acquirer acquirer = new Acquirer(this);
+
+    public void runOpMode() throws InterruptedException{
+        drive.init(hardwareMap);
+        platform.init(hardwareMap);
+        acquirer.init(hardwareMap);
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        waitForStart();
+        platform.platformUp();
+        sleep(500);
+        drive.strafeRight();
+        sleep(600);
+        drive.driveToPos(distance, .5);
+        platform.platformDown();
+        sleep(500);
+        drive.driveToPos(-distance -1, .5);
+        platform.platformUp();
+        sleep(1000);
+        acquirer.acquirerUp();
+        sleep(500);
+        drive.strafeLeft ();
+        sleep(2000);
+        drive.driveToPos(18, .5);
+        drive.strafeLeft ();
+        sleep(800);
+//        platform.platformUp();
+//        drive.strafeLeft();
+//        sleep(time2);
+    }
+}
