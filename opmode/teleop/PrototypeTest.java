f12e5705be95a149ddbed4554eb9290dec03c934
jasonjlam <amsjlam@gmail.com>
2019-12-19 17:50:17 -0500

    tested prototype acquirer

---
+++ opmode/teleop/PrototypeTest.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode.opmode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.teamcode.hardware.Arm;
+import org.firstinspires.ftc.teamcode.hardware.Platform;
+import org.firstinspires.ftc.teamcode.hardware.PrototypeAcquirer;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@Config
+@TeleOp(name="PrototypeTest", group="Teleop")
+public class PrototypeTest extends LinearOpMode {
+    public static double leftAngle = 0;
+    public static double rightAngle = 0;
+
+    PrototypeAcquirer acquirer = new PrototypeAcquirer(this);
+    public void runOpMode() {
+        acquirer.init(hardwareMap);
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        waitForStart();
+        while (opModeIsActive()) {
+            if (gamepad1.a) {
+                acquirer.gripOpen();
+            }
+            else if (gamepad1.b) {
+                acquirer.gripClose();
+            }
+            if (gamepad1.x) {
+                acquirer.acquirerIn();
+            }
+            if (gamepad1.y) {
+                acquirer.acquirerOut();
+            }
+
+            else {
+                acquirer.acquirerOff();
+            }
+
+        }
+    }
+}
