72930084616daf20115c009ba931c42b2427bb5f
jasonjlam <amsjlam@gmail.com>
2020-01-03 09:19:17 -0500

    might fix some issues with switch not being detectd

--- hardware/Switch.java
+++ hardware/Switch.java
@@ -1,21 +1,20 @@
 package org.firstinspires.ftc.teamcode.hardware;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.TouchSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 public class Switch extends Mechanism {
-    private DigitalChannel limitSwitch;  // Hardware Device Object
+    private TouchSensor limitSwitch;  // Hardware Device Object
 
     public Switch() {}
 
     public Switch(LinearOpMode opMode){ this.opMode = opMode; }
 
     public void init(HardwareMap hwMap) {
-        limitSwitch = hwMap.digitalChannel.get("switch");
-        limitSwitch.setMode(DigitalChannel.Mode.INPUT);
+        limitSwitch = hwMap.touchSensor.get("switch");
     }
 
     public boolean isPressed() {
-        return limitSwitch.getState();
+        return limitSwitch.isPressed();
     }
 }
 
