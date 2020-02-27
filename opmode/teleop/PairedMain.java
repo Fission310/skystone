2c66966252880d074f69ccaebb9284909a491d45
Lucas Lee <lucasclee11@gmail.com>
2020-02-26 23:00:28 -0500

    more minor cleanup

--- opmode/teleop/PairedMain.java
+++ opmode/teleop/PairedMain.java
@@ -14,9 +14,7 @@ import org.firstinspires.ftc.teamcode.hardware.Lift;
 @TeleOp(name="PairedMain", group="Main")
 public class PairedMain extends LinearOpMode {
 
-    double leftInput1, rightInput1, slideInput1, leftInput2, rightInput2, slideInput2;
-//    Unused; fix later;
-    double armPosition = 0.5;
+    private double leftInput1, rightInput1, slideInput1, leftInput2, rightInput2, slideInput2;
     private Drivetrain drive = new Drivetrain(this);
     private Acquirer acquirer = new Acquirer(this);
     private Tape parker = new Tape(this);
@@ -26,7 +24,7 @@ public class PairedMain extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
-//        Initializing
+        //Initializing
         acquirer.init(hardwareMap);
         drive.init(hardwareMap);
         parker.init(hardwareMap);
@@ -38,31 +36,34 @@ public class PairedMain extends LinearOpMode {
             telemetry.addData("Status", "Waiting in Init");
             telemetry.update();
         }
-
         waitForStart();
 
         while(opModeIsActive()) {
-//            Inputs for the stick and triggers
-//            Sticks are [0,1], triggers are [-1,1] as a sum
-//            leftInput1 = gamepad1.left_stick_y;
-//            rightInput1 = gamepad1.right_stick_y;
+            //Inputs for the stick and triggers
+            // Sticks are [0,1], triggers are [-1,1] as a sum
+            leftInput1 = gamepad1.left_stick_y;
+            rightInput1 = gamepad1.right_stick_y;
             slideInput1 = -gamepad1.left_trigger + gamepad1.right_trigger;
-//            Second controller
+
+            //Second controller
             leftInput2 = -gamepad2.left_stick_y;
             rightInput2 = -gamepad2.right_stick_y;
             slideInput2 = -gamepad2.left_trigger + gamepad2.right_trigger;
-//            How far the stick goes
+
+            //How far the stick goes
             double r = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);
-//            scuffed way to smooth inputs by cubing
+
+            //\scuffed way to smooth inputs by cubing
             r = Math.pow(r,3);
             leftInput2 = Math.pow(leftInput2, 3);
-//            Angle of the stick
+
+            //Angle of the stick
             double robotAngle = Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
-//            How far the right stick goes from side to side (turning)
+
+            //How far the right stick goes from side to side (turning)
             double rightX1 = gamepad1.right_stick_x;
 
-//            Controller 1: Driver
-//            Precision mode if trigger is held, else left stick is vector driving and right stick is turning
+            // Controller 1: Driver
             if (gamepad1.dpad_left) drive.strafeLeft();
             else if (gamepad1.dpad_right) drive.strafeRight();
             if (slideInput1 > 0.3) drive.teleDrive(r/2, robotAngle, rightX1/3);
@@ -77,16 +78,13 @@ public class PairedMain extends LinearOpMode {
             else if(gamepad1.dpad_down) parker.retract();
             else parker.stop();
 
-            if(gamepad1.dpad_left) leg.kick();
-            if (gamepad1.dpad_right) leg.back();
+            if(gamepad1.left_bumper) leg.kick();
+            if (gamepad1.right_bumper) leg.back();
 
             if(gamepad1.x) lift.up();
             else if (gamepad1.y) lift.down();
             else lift.stop();
 
-
-
-//            Random telemetry for testing purposes
             telemetry.addData("slide", slideInput1);
             telemetry.addData("righttrigger", gamepad1.right_trigger);
             telemetry.addData("rightx1", rightX1);
